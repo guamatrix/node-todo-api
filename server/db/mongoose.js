b98fd754c00b17cfc09fc2474ba7ae4b42be814c
Juan Bautista Marval Hernandez <juan.marval@globant.com>
2018-05-29 11:50:28 -0300

    test get todos routes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ const URL_DB = 'mongodb://localhost:27017/TodoApp';
 // const URL_DB = 'mongodb://guamatrix:guamatrix@ds137740.mlab.com:37740/todo-dev-db';
 
 mongoose.connect(URL_DB).then(resp => {
-  console.log('Connectd db');
+  // console.log('Connectd db');
 }).catch(err => {
   console.log('error: ', err);
 });
