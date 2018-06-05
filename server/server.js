ea33cd928bc9577df2fda09920a9188048d8d587
Juan Bautista Marval Hernandez <juan.marval@globant.com>
2018-06-05 11:05:22 -0300

    allow headers

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
+require('./db/mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth');
+  res.header('Access-Control-Allow-Methods: GET, POST, PATCH');
+  next();
+}); 
 
 require('./routes/todosRoutes')(app);
 require('./routes/usersRoutes')(app);
