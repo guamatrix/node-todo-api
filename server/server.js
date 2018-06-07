31e83f01939dadca01f5a403d88ceec0cbee0360
Juan Bautista Marval Hernandez <juan.marval@globant.com>
2018-06-07 19:16:25 -0300

    adding options

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth');
   res.header('Access-Control-Expose-Headers', 'x-auth');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
 }); 
 
